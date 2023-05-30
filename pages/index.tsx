be72a558791be9468b3e0437891fad50729eca71
jjoyrb <106985837+jjoyrb@users.noreply.github.com>
2023-05-29 23:27:23 -0400

    Added Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Components
+import { BackgroundImage1,BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon, RedSpan} from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Assets
+import Clouds1 from '@/assets/cloud-and-thunder.png'
+import Clouds2 from '@/assets/cloudy-weather.png'
 
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -15,7 +24,27 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
-        
+
+        <BackgroundImage1
+        src={Clouds1}
+        height="300"
+        alt="cloudybackground1"
+        />
+
+      <BackgroundImage2
+        src={Clouds2}
+        height="300"
+        alt="cloudybackground1"
+        />
+
+      <FooterCon>
+        <>
+          Quotes Generated: {numberOfQuotes}
+          <br/>
+          Developed with <RedSpan>❤️</RedSpan> by <FooterLink href="https://www.linkedin.com/in/johnjoy/" target="_blank" rel="noopener noreferrer">@JohnJoy</FooterLink>
+        </>
+      </FooterCon>
+
       </GradientBackgroundCon>
     </>
   )
